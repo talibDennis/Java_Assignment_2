18094fb27a4322ba0094f55d5b39de95067fe327
talibDennis <joseph.dennis@triosstudent.com>
2025-07-25 06:48:52 -0400

    Finished Cow.java

--- animal/Cow.java
+++ animal/Cow.java
@@ -2,5 +2,21 @@ package animal;
 
 public class Cow extends Animal {
   private int milkPerDay;
-  
-}
+
+  // Explicit constructor that calls 
+  public Cow(String type) {
+    super(type);
+    this.milkPerDay = super.randNum.nextInt(701) + 50; // 50-701
+  } // constructor
+
+  // getters
+  public int getMilkPerDay() {
+    return this.milkPerDay;
+  }
+
+  // setters
+  public void setMilkPerDay(int milkPerDay) {
+    this.milkPerDay = milkPerDay;
+  }
+
+} // clas
